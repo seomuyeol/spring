92c7c03d851d4376c9516855541994712023fda2
SEO-my <muyear.seo@gmail.com>
2022-04-27 16:12:50 +0900

    ブランク修正

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -9,7 +9,7 @@ public class RateDiscountPolicy implements DiscountPolicy {
 	
 	@Override
 	public int discount(Member member, int price) {
-		if (member.getGrade() == Grade.VIP	 ) {
+		if (member.getGrade() == Grade.VIP) {
 			return price * discountPercent / 100;
 		} else {
 			return 0;
